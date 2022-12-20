5ef171c388ee7e844924d44b8e46b8fb4a8bc407
r-clifford <r-clifford>
2022-12-19 22:14:21 -0500

    2a

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,38 +1,66 @@
-use std::num::ParseIntError;
-use std::path::PathBuf;
-use std::io::{self, BufReader, BufRead};
+use std::collections::HashMap;
 use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::ops::Sub;
+use std::path::PathBuf;
 use std::str::FromStr;
-
-fn solution2(path: PathBuf) -> i32 {
-    let lines = BufReader::new(File::open(path).unwrap()).lines();
-    let mut count = 0;
-    let mut counts = vec![];
-
-    for line in lines {
-        match i32::from_str_radix(&line.unwrap(), 10) {
-            Ok(i) => {
-                count += i;
-            },
-            Err(_) => {
-                counts.push(count);
-                count = 0;
-            }
+#[derive(Debug, Clone, Copy)]
+enum PlayResult {
+    Win = 6,
+    Loss = 0,
+    Draw = 3,
+}
+#[derive(Debug, Clone, Copy)]
+enum Plays {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+impl Plays {
+    fn from_char(c: &char) -> Self {
+        match c {
+            'A' | 'X' => Plays::Rock,
+            'B' | 'Y' => Plays::Paper,
+            'C' | 'Z' => Plays::Scissors,
+            _ => panic!("Invalid play"),
         }
     }
-    counts.sort();
-    let mut result = 0;
-    let mut iter = counts.iter().rev();
-    for _ in 0..3 {
-        let i = iter.next().unwrap();
-        result += i;
+    fn test(&self, opponent: &Self) -> PlayResult {
+        match *self - *opponent {
+            0 => PlayResult::Draw,
+            -1 | 2 => PlayResult::Loss,
+            -2 | 1 => PlayResult::Win,
+            _ => panic!("Invalid play"),
+        }
     }
-    return result;
 }
-pub fn test2() {
-    let path = match PathBuf::from_str("./src/data/1.txt") {
-        Ok(p) => p,
-        Err(e) => panic!("Invalid path"),
-    };
-    println!("{}", solution2(path));
+impl Sub for Plays {
+    type Output = i32;
+    fn sub(self, rhs: Self) -> Self::Output {
+        (self as i32) - (rhs as i32)
+    }
+}
+
+fn calculate_score(path: PathBuf) -> i32 {
+    let lines = io::BufReader::new(File::open(path).unwrap()).lines();
+    let mut total = 0;
+    for line in lines {
+        let line = line.unwrap();
+        let split = line.split(" ").collect::<Vec<&str>>();
+        let player = Plays::from_char(&split[1].chars().next().unwrap());
+        let opponent = Plays::from_char(&split[0].chars().next().unwrap());
+        let result = player.test(&opponent);
+        let score = (result as i32) + (player as i32);
+        total += score;
+    }
+    return total;
+}
+
+fn solution2() {
+    let path = PathBuf::from_str("./src/data/2.txt").unwrap();
+    print!("{}", calculate_score(path));
+}
+pub fn test2a() {
+    solution2();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod day_1;
+mod day_1b;
 mod day_2;
 fn main() {
-    println!("Hello, world!");
     day_1::test1();
-    day_2::test2();
+    day_1b::test1b();
+    day_2::test2a();
 }
